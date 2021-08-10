a5c8e478a0f5e9f6a3d4baf6786e730e484c4814
Wasiim Hosenbocus <wasiim.hosenbocus@outlook.com>
2021-08-10 20:32:25 +0400

    Added Cosmos DB + moved locality to settings

---
+++ CEBAlert/Model/Alert.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CEBAlert.Model
+{
+    public class Alert
+    {
+        public string OutageTime { get; set; }
+        public string Locality { get; set; }
+        public string LocalityDetails { get; set; }
+    }
+}
